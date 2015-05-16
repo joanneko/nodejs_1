e8b2d201f8b0bd83084f428a6a02ab56351695e4
joanne <jojoannexo@gmail.com>
2015-05-16 17:04:55 +0530

    response

--- router.js
+++ router.js
@@ -1,14 +1,16 @@
-function route (handle, pathname) {
+function route (handle, pathname, response) {
 	console.log("About to route a request for " + pathname);
 	if (typeof handle[pathname] === 'function') {
 		// [Function: start] 
 		// console.log(handle[pathname]); 
 		// / 
 		// console.log(pathname);
-		console.log("<33333")
-		handle[pathname](); 
+		handle[pathname](response); 
 	} else {
 		console.log("No request handler found for " + pathname); 
+		response.writeHead(404, {"Content-Type": "text/plain"}); 
+		response.write("404 not found");
+		response.end(); 
 	}
 }
 
